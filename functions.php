8a5ee5d8e53344b7e400a8ebf6fbdc60f63d7a3c
Steve Mynett <steve@mynettworks.com>
2011-10-19 16:36:04 -0700

    Changed jQuery embed method
    Altered to call via functions and removed the hard patched version in
    header.

--- functions.php
+++ functions.php
@@ -14,6 +14,25 @@ if ( function_exists('register_sidebar') )
 		'after_title' => '</h4>',
 ));
 
+
+
+// embed jquery via Google's CDN
+function skm_init_method() {
+	/* Enqueue custom Javascript here using wp_enqueue_script(). */
+	if ( !is_admin() ) {
+		  wp_deregister_script('jquery');
+		  wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"), false);
+		  wp_enqueue_script('jquery');
+	}
+
+	/* Load the comment reply JavaScript. */
+	if ( is_singular() && get_option( 'thread_comments' ) && comments_open() )
+		wp_enqueue_script( 'comment-reply' );
+}
+
+add_action('wp_enqueue_scripts', 'skm_init_method');
+
+
 function excerpt($limit) {
 	$excerpt = explode(' ', get_the_content(), $limit);
 	if (count($excerpt)>=$limit) {
