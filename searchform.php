8806beeed4510ae441b39061fc9a5f85dbe99617
Steve Mynett <steve@mynettworks.com>
2011-10-21 01:48:03 -0700

    Updated to HTML5 Search Form
    http://bavotasan.com/2011/html5-search-form-in-wordpress/

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <nav>
-	<form action="<?php echo home_url(); ?>" id="searchform" method="get">
-		<label for="s" class="screen-reader-text">Search for:</label>
-	    <input class="text" type="text" id="s" name="s" value="" />
-	    <input class="submit" type="submit" value="Search" id="searchsubmit" />
+	<form role="search" method="get" id="searchform" action="<?php echo home_url(); ?>">
+	    <label class="assistive-text" for="s">Search for:</label>
+	    <input type="search" placeholder="Enter term..." value="<?php get_search_query(); ?>" name="s" id="s">
+	    <input type="submit" id="searchsubmit" value="Search">
 	</form>
 </nav>
