b86715129ead2057c6a19c0442c9454dfe9f2929
Steve Mynett <steve@mynettworks.com>
2012-01-29 14:38:24 -0800

    Updated Modernizr

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<script src="<?php bloginfo('template_url'); ?>/js/modernizr.2.0.6.custom.js"></script>
-	<?php wp_enqueue_script('jquery', '/wp-content/themes/sm_framework/js/jquery-1.6.2.min.js', array('jquery')); ?>	
 	
 	<?php if ( is_singular() ) echo '<link rel="canonical" href="' . get_permalink() . '">'; ?>
 
